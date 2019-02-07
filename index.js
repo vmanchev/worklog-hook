5802afda604a70467cf3d418f3480028e43712d4
Venelin Manchev <venelinphp@gmail.com>
2019-02-07 06:51:27 +0200

    fix: replaced parse-git-config with gitconfig

--- index.js
+++ index.js
@@ -1,115 +1,134 @@
 #!/usr/bin/env node
 
-const inquirer = require('inquirer');
-const parsrGitConfig = require('parse-git-config');
-const request = require('request');
-const childProcessExec = require('child_process').exec;
-const util = require('util');
+const inquirer = require("inquirer");
+const gitconfig = require("gitconfig");
+const request = require("request");
+const childProcessExec = require("child_process").exec;
+const util = require("util");
 const exec = util.promisify(childProcessExec);
-
-let gitConfig = getGitConfig().worklog;
 
 const hookTypeOptions = [
   {
-    name: 'pre-commit (terminate operation)',
-    value: 'pre-commit'
+    name: "pre-commit (terminate operation)",
+    value: "pre-commit"
   },
   {
-    name: 'post-commit (warning only)',
-    value: 'post-commit'
+    name: "post-commit (warning only)",
+    value: "post-commit"
   },
   {
-    name: 'pre-push  (terminate operation)',
-    value: 'pre-push'
+    name: "pre-push  (terminate operation)",
+    value: "pre-push"
   }
 ];
 
 const versionOptions = [
   {
-    name: '2 (current stable)',
-    value: '2'
+    name: "2 (current stable)",
+    value: "2"
   },
   {
-    name: '3 (experimental)',
-    value: '3'
+    name: "3 (experimental)",
+    value: "3"
   }
 ];
 
 const setupQuestions = [
   {
-    type: 'input',
-    name: 'email',
-    message: 'Jira login email:',
-    default: gitConfig ? gitConfig.email : null,
+    type: "input",
+    name: "email",
+    message: "Jira login email:",
+    // default: gitConfig ? gitConfig.email : null,
     validate: email => !!email.length && /\w+@\w+/.test(email)
   },
   {
-    type: 'password',
-    name: 'password',
-    message: 'Jira login password:',
+    type: "password",
+    name: "password",
+    message: "Jira login password:",
     validate: password => !!password.length
   },
   {
-    type: 'password',
-    name: 'confirmPassword',
-    message: 'Confirm password:',
+    type: "password",
+    name: "confirmPassword",
+    message: "Confirm password:",
     validate: (confirmPassword, answers) =>
       !!confirmPassword.length && confirmPassword == answers.password
   },
   {
-    type: 'list',
-    name: 'type',
-    message: 'What type of git hook you want to use for worklog?',
+    type: "list",
+    name: "type",
+    message: "What type of git hook you want to use for worklog?",
     choices: hookTypeOptions,
-    default: gitConfig ? getIndex(hookTypeOptions, gitConfig.type) : null
+    // default: gitConfig ? getIndex(hookTypeOptions, gitConfig.type) : null
   },
   {
-    type: 'input',
-    name: 'url',
-    message: 'Full Jira board url:',
-    default: gitConfig ? gitConfig.url : null,
+    type: "input",
+    name: "url",
+    message: "Full Jira board url:",
+    // default: gitConfig ? gitConfig.url : null,
     validate: url => /^https:\/\//.test(url)
   },
   {
-    type: 'list',
-    name: 'version',
-    message: 'Which Jira REST API version your team is using?',
+    type: "list",
+    name: "version",
+    message: "Which Jira REST API version your team is using?",
     choices: versionOptions,
-    default: gitConfig ? getIndex(versionOptions, gitConfig.version) : null
+    // default: gitConfig ? getIndex(versionOptions, gitConfig.version) : null
   }
 ];
 
-if (!gitConfig) {
-  inquirer.prompt(setupQuestions).then(answers => {
-    gitConfig = answers ? answers : {};
+gitconfig
+  .get({
+    location: "local"
+  })
+  .then(data => {
+    let worklogConfig = data.worklog;
 
-    exec(`git config worklog.email ${gitConfig.email}`);
-    exec(`git config worklog.password ${gitConfig.password}`);
-    exec(`git config worklog.type ${gitConfig.type}`);
-    exec(`git config worklog.url ${gitConfig.url}`);
-    exec(`git config worklog.version ${gitConfig.version}`);
-
-    getJiraCode()
-      .then(branch => sendRequest(branch))
-      .catch(e => console.log(e.message));
+    // if no config, start the setup process
+    if (!worklogConfig) {
+      runConfig().then(worklogConfig => {
+        checkWorkLog(worklogConfig)
+      });
+    } else {
+      checkWorkLog(worklogConfig)
+    }
+    
   });
-} else {
+
+function runConfig() {
+  return inquirer
+    .prompt(setupQuestions)
+    .then(answers => updateConfigFile(answers));
+}
+
+function updateConfigFile(worklogConfig) {
+  return gitconfig.set(
+    {
+      worklog: worklogConfig
+    },
+    {
+      location: "local"
+    }
+  ).then(_ => worklogConfig);
+}
+
+function checkWorkLog(worklogConfig) {
   getJiraCode()
-    .then(branch => sendRequest(branch))
+    .then(branch => sendRequest(branch, worklogConfig))
     .catch(e => console.log(e.message));
 }
 
-function sendRequest(branch) {
+function sendRequest(branch, worklogConfig) {
   try {
     getIssue(
       branch,
-      gitConfig.url,
-      gitConfig.version,
-      gitConfig.email,
-      gitConfig.password
-    )
+      worklogConfig.url,
+      worklogConfig.version,
+      worklogConfig.email,
+      worklogConfig.password
+    );
   } catch (e) {
-    console.error(e.message)
+    console.error(e.message);
   }
 }
 
@@ -117,27 +136,20 @@ function sendRequest(branch) {
  * Get Jira issue code out of current branch name
  */
 async function getJiraCode() {
-  const branches = await exec('git branch');
+  const branches = await exec("git branch");
   const parseBranch = branches.stdout
-    .split('\n')
-    .find(b => b.charAt(0) === '*')
+    .split("\n")
+    .find(b => b.charAt(0) === "*")
     .trim()
     .substring(2);
-  return 'DN-3815';
+  return "DN-3815";
   if (parseBranch && parseBranch.match(/^\w+-\d+/)) {
     return parseBranch.match(/^\w+-\d+/)[0];
   }
 
   throw new Error(
-    'Branch name ' + parseBranch + ' does not match naming convention'
+    "Branch name " + parseBranch + " does not match naming convention"
   );
-}
-
-/**
- * Get local .git/config and parse it as JSON
- */
-function getGitConfig() {
-  return parsrGitConfig.sync();
 }
 
 /**
@@ -151,19 +163,18 @@ function getIndex(collection, value) {
 }
 
 function getIssue(branch, url, version, email, password) {
-
-  console.log('='.repeat(50));
+  console.log("=".repeat(50));
   console.log(`Verifying worklog for task ${branch}`);
-  console.log('='.repeat(50));
+  console.log("=".repeat(50));
 
-  var credentials = Buffer.from(email + ':' + password).toString('base64');
+  var credentials = Buffer.from(email + ":" + password).toString("base64");
 
   var options = {
-    method: 'GET',
+    method: "GET",
     url: `${url}/rest/api/${version}/issue/${branch}/worklog`,
     headers: {
-      Authorization: 'Basic ' + credentials,
-      Accept: 'application/json'
+      Authorization: "Basic " + credentials,
+      Accept: "application/json"
     }
   };
 
@@ -173,17 +184,17 @@ function getIssue(branch, url, version, email, password) {
     const wl = JSON.parse(body);
 
     console.log(`Registered worklogs: ${wl.total}`);
-    console.log('-'.repeat(50));
+    console.log("-".repeat(50));
 
     if (wl.total) {
       wl.worklogs.forEach(work => {
         console.log(`${work.author.displayName}\t${work.timeSpent}`);
       });
 
-      console.log('-'.repeat(50));
+      console.log("-".repeat(50));
     } else {
-      console.error('You must log time for this task!');
-      console.log('^'.repeat(50));
+      console.error("You must log time for this task!");
+      console.log("^".repeat(50));
     }
   });
 }
